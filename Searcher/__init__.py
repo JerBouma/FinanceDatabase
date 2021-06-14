64c48671668c5e29932de8ceea9773a330fd9b92
Jeroen Bouma <jer.bouma@gmail.com>
2021-06-14 17:23:24 +0200

    Small fix for 0.1.9

--- Searcher/__init__.py
+++ Searcher/__init__.py
@@ -5,7 +5,7 @@ from .json_picker import select_etfs
 from .json_picker import select_equities
 from .json_picker import select_funds
 from .json_picker import select_indices
-from .json_picker import select_other
+from .json_picker import select_moneymarkets
 
 from .json_options import show_options
 from .json_options import search_products
