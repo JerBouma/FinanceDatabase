825ab735316b3c42caf1937b2625b68013d1fdd9
Jeroen Bouma <jer.bouma@gmail.com>
2021-06-28 17:43:27 +0200

    Add new example for show_options

---
+++ Examples/CountrySectorIndustryExample.py
@@ -0,0 +1,31 @@
+import FinanceDatabase as fd
+import matplotlib.pyplot as plt
+
+
+# Obtain all countries from the database
+equities_countries = fd.show_options('equities', 'countries')
+
+# Obtain all sectors from the database
+equities_sectors = fd.show_options('equities', 'sectors')
+
+# Obtain all industries from the database
+equities_industries = fd.show_options('equities', 'industries')
+
+# Obtain all industries from the database
+equities_all_categories = fd.show_options('equities')
+
+equities_per_sector_netherlands = {}
+
+for sector in equities_sectors[1:]:  # I skip the first sector since it means 'No Category'
+    try:
+        equities_per_sector_netherlands[sector] = len(fd.select_equities(country='Netherlands', sector=sector))
+    except ValueError as error:
+        print(error)
+
+legend, values = zip(*equities_per_sector_netherlands.items())
+
+plt.pie(values, labels=legend)
+plt.title('Companies per sector in the Netherlands')
+plt.tight_layout()
+
+plt.show()
