1e34ff99fe87fb70397b5a4e261c47877e2ce44f
Jeroen Bouma <jer.bouma@gmail.com>
2021-10-18 08:18:13 +0200

    Update to version v0.1.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="FinanceDatabase",
     packages=["FinanceDatabase"],
-    version="0.1.10",
+    version="0.1.11",
     license="MIT",
     description="This is a database of 300.000+ symbols containing Equities, ETFs, Funds, Indices, "
                 "Currencies, Cryptocurrencies and Money Markets.",
