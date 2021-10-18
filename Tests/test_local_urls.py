cea5ccb3318def4c2db8a62348f9f000765cec5b
Jeroen Bouma <jer.bouma@gmail.com>
2021-10-18 08:12:02 +0200

    Update tests due to new parameter in json_picker

--- Tests/test_local_urls.py
+++ Tests/test_local_urls.py
@@ -11,15 +11,15 @@ def test_local_urls():
     assert type(crypto) is dict and len(crypto) > 0, "crypto dictionary is invalid."
 
     etfs = select_etfs(base_url=rf'{LOCAL_URL}/ETFs/',
-                       use_local_location=True)
+                       use_local_location=True, exclude_exchanges=False)
     assert type(etfs) is dict and len(etfs) > 0, "etfs dictionary is invalid."
 
     equity = select_equities(base_url=rf'{LOCAL_URL}/Equities/',
-                             use_local_location=True)
+                             use_local_location=True, exclude_exchanges=False)
     assert type(equity) is dict and len(equity) > 0, "equity dictionary is invalid."
 
     funds = select_funds(base_url=rf'{LOCAL_URL}/Funds/',
-                         use_local_location=True)
+                         use_local_location=True, exclude_exchanges=False)
     assert type(funds) is dict and len(funds) > 0, "funds dictionary is invalid."
 
     currency = select_currencies(base_url=rf'{LOCAL_URL}/Currencies/',
@@ -27,11 +27,11 @@ def test_local_urls():
     assert type(currency) is dict and len(currency) > 0, "currency dictionary is invalid."
 
     index = select_indices(base_url=rf'{LOCAL_URL}/Indices/',
-                           use_local_location=True)
+                           use_local_location=True, exclude_exchanges=False)
     assert type(index) is dict and len(index) > 0, "index dictionary is invalid."
 
     moneymarket = select_moneymarkets(base_url=rf'{LOCAL_URL}/Moneymarkets/',
-                                      use_local_location=True)
+                                      use_local_location=True, exclude_exchanges=False)
     assert type(moneymarket) is dict and len(moneymarket) > 0, "moneymarket dictionary is invalid."
 
 
--- Tests/test_local_urls_specific.py
+++ Tests/test_local_urls_specific.py
@@ -13,42 +13,47 @@ def test_local_urls_specific():
 
     etfs = select_etfs(category='China Region',
                        base_url=rf'{LOCAL_URL}/ETFs/',
-                       use_local_location=True)
+                       use_local_location=True, exclude_exchanges=False)
     assert type(etfs) is dict and len(etfs) > 0, "etfs China Region dictionary is invalid."
 
     equity = select_equities(country='Canada',
                              base_url=rf'{LOCAL_URL}/Equities/',
-                             use_local_location=True)
+                             use_local_location=True, exclude_exchanges=False)
     assert type(equity) is dict and len(equity) > 0, "equity Canada dictionary is invalid."
 
     equity = select_equities(sector='Energy',
                              base_url=rf'{LOCAL_URL}/Equities/',
-                             use_local_location=True)
+                             use_local_location=True, exclude_exchanges=False)
     assert type(equity) is dict and len(equity) > 0, "equity Energy dictionary is invalid."
 
     equity = select_equities(industry='Biotechnology',
                              base_url=rf'{LOCAL_URL}/Equities/',
-                             use_local_location=True)
+                             use_local_location=True, exclude_exchanges=False)
     assert type(equity) is dict and len(equity) > 0, "equity Biotechnology dictionary is invalid."
 
     equity = select_equities(country='United States', sector='Financial',
                              base_url=rf'{LOCAL_URL}/Equities/',
-                             use_local_location=True)
+                             use_local_location=True, exclude_exchanges=False)
     assert type(equity) is dict and len(equity) > 0, "equity United States + Financial dictionary is invalid."
 
     equity = select_equities(country='Slovakia', industry='Resorts & Casinos',
                              base_url=rf'{LOCAL_URL}/Equities/',
-                             use_local_location=True)
+                             use_local_location=True, exclude_exchanges=False)
     assert type(equity) is dict and len(equity) > 0, "equity Slovakia + Resorts & Casinos dictionary is invalid."
+
+    with pytest.raises(ValueError):
+        equity = select_equities(country='Slovakia', industry='Resorts & Casinos',
+                                 base_url=rf'{LOCAL_URL}/Equities/',
+                                 use_local_location=True, exclude_exchanges=True)
 
     equity = select_equities(country='Panama', sector='Industrials', industry='Airlines',
                              base_url=rf'{LOCAL_URL}/Equities/',
-                             use_local_location=True)
+                             use_local_location=True, exclude_exchanges=False)
     assert type(equity) is dict and len(equity) > 0, "equity Panama + Industrials + Airlines is invalid."
 
     funds = select_funds(category='Alt - Currency',
                          base_url=rf'{LOCAL_URL}/Funds/',
-                         use_local_location=True)
+                         use_local_location=True, exclude_exchanges=False)
     assert type(funds) is dict and len(funds) > 0, "funds Alt - Currency dictionary is invalid."
 
     currency = select_currencies(currency='EUR',
@@ -58,12 +63,12 @@ def test_local_urls_specific():
 
     index = select_indices(market='cn_market',
                            base_url=rf'{LOCAL_URL}/Indices/',
-                           use_local_location=True)
+                           use_local_location=True, exclude_exchanges=False)
     assert type(index) is dict and len(index) > 0, "index cn_market dictionary is invalid."
 
     moneymarket = select_moneymarkets(market='us_market',
                                       base_url=rf'{LOCAL_URL}/Moneymarkets/',
-                                      use_local_location=True)
+                                      use_local_location=True, exclude_exchanges=False)
     assert type(moneymarket) is dict and len(moneymarket) > 0, "moneymarket us_market dictionary is invalid."
 
 
