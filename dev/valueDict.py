6090777d328208da752a53a601859d2401550ac1
fred3ric <fredreinier@gmail.com>
2022-10-23 17:40:16 +0200

    add test

---
+++ dev/valueDict.py
@@ -0,0 +1,10 @@
+listPEA = {"Axa":"CS.PA",
+           "CreditAgricole":"ACA.PA",
+           "EDF":"EDF.PA", 
+           "Engie":"ENGI.PA", 
+           "Euroapi":"EAPI.PA",
+           "FDJ":"FDJ.PA",
+           "NamR":"ALNMR.PA",
+           "TotalEnergies":"TTE.PA",
+           "Vinci":"DG.PA"}
+#           "SP500ETF":"PSP5.PA"}
