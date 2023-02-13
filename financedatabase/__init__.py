672ba677eec3559d78afa98b3f54776ecc35e04d
jeroen <jer.bouma@gmail.com>
2023-02-13 21:20:59 +0100

    oops 2

--- financedatabase/__init__.py
+++ financedatabase/__init__.py
@@ -6,6 +6,7 @@ from .json_picker import select_equities
 from .json_picker import select_funds
 from .json_picker import select_indices
 from .json_picker import select_moneymarkets
+from .json_picker import exclude_exchange
 
 from .json_options import show_options
 from .json_options import search_products
