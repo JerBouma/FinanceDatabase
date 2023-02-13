5107ec61ee45bd8a5069719cffeabbac35aeb12e
jeroen <jer.bouma@gmail.com>
2023-02-13 22:22:34 +0100

    Inefficent fixing

--- financedatabase/__init__.py
+++ financedatabase/__init__.py
@@ -1,12 +1,12 @@
 # Modules
-from .json_picker import select_cryptocurrencies
-from .json_picker import select_currencies
-from .json_picker import select_etfs
-from .json_picker import select_equities
-from .json_picker import select_funds
-from .json_picker import select_indices
-from .json_picker import select_moneymarkets
-from .json_picker import exclude_exchange
+from .json_picker import select_cryptocurrencies  # noqa
+from .json_picker import select_currencies  # noqa
+from .json_picker import select_etfs  # noqa
+from .json_picker import select_equities  # noqa
+from .json_picker import select_funds  # noqa
+from .json_picker import select_indices  # noqa
+from .json_picker import select_moneymarkets  # noqa
+from .json_picker import exclude_exchange  # noqa
 
-from .json_options import show_options
-from .json_options import search_products
+from .json_options import show_options  # noqa
+from .json_options import search_products  # noqa
