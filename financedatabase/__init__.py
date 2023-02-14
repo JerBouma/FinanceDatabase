e296b7272432f313e827c901e79e21ca45b44c8b
colin99d <colin99delahunty@gmail.com>
2023-02-14 09:52:10 -0500

    Finished conversion of equities

--- financedatabase/__init__.py
+++ financedatabase/__init__.py
@@ -6,7 +6,6 @@ from .equities import Equities  # noqa
 from .json_picker import select_funds  # noqa
 from .json_picker import select_indices  # noqa
 from .json_picker import select_moneymarkets  # noqa
-from .json_picker import exclude_exchange  # noqa
 
 from .json_options import show_options  # noqa
 from .json_options import search_products  # noqa
