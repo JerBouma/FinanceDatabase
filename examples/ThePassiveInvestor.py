13c0e2cc423cfe2af5fe96326c0cbbd989fc981c
jeroen <jer.bouma@gmail.com>
2023-02-15 23:04:17 +0100

    Remove examples folder

--- examples/ThePassiveInvestor.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-import financedatabase as fd
-
-core_selection = fd.select_etfs("core_selection_filtered", exclude_exchanges=False)
-
-tickers = pd.Series(core_selection.keys())
-tickers.to_excel("core_selection_tickers.xlsx", index=None, header=None)
