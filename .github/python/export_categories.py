b2cdd798c758556a6a3e7241ee065c7024f4d0ca
jeroen <jer.bouma@gmail.com>
2023-02-22 22:47:29 +0100

    Take 6

--- .github/python/export_categories.py
+++ .github/python/export_categories.py
@@ -1,17 +1,18 @@
 import financedatabase as fd
+import pandas as pd
 
 equities = fd.Equities()
-equities.show_options('countries').to_csv('Database/Categories/equities_countries.csv')
-equities.show_options('sectors').to_csv('Database/Categories/equities_sectors.csv')
-equities.show_options('industry_groups').to_csv('Database/Categories/equities_industry_groups.csv')
-equities.show_options('industries').to_csv('Database/Categories/equities_industries.csv')
+pd.Series(equities.options('country')).to_csv('Database/Categories/equities_countries.csv', index=False, header=False)
+pd.Series(equities.options('sector')).to_csv('Database/Categories/equities_sectors.csv', index=False, header=False)
+pd.Series(equities.options('industry_group')).to_csv('Database/Categories/equities_industry_groups.csv', index=False, header=False)
+pd.Series(equities.options('industry')).to_csv('Database/Categories/equities_industries.csv', index=False, header=False)
 
 etfs = fd.ETFs()
-etfs.show_options('category_group').to_csv('Database/Categories/etfs_category_group.csv')
-etfs.show_options('category').to_csv('Database/Categories/etfs_category.csv')
-etfs.show_options('family').to_csv('Database/Categories/etfs_family.csv')
+pd.Series(etfs.options('category_group')).to_csv('Database/Categories/etfs_category_group.csv', index=False, header=False)
+pd.Series(etfs.options('category')).to_csv('Database/Categories/etfs_category.csv', index=False, header=False)
+pd.Series(etfs.options('family')).to_csv('Database/Categories/etfs_family.csv', index=False, header=False)
 
 funds = fd.Funds()
-funds.show_options('category_group').to_csv('Database/Categories/funds_category_group.csv')
-funds.show_options('category').to_csv('Database/Categories/funds_category.csv')
-funds.show_options('family').to_csv('Database/Categories/funds_family.csv')
+pd.Series(funds.options('category_group')).to_csv('Database/Categories/funds_category_group.csv', index=False, header=False)
+pd.Series(funds.options('category')).to_csv('Database/Categories/funds_category.csv', index=False, header=False)
+pd.Series(funds.options('family')).to_csv('Database/Categories/funds_family.csv', index=False, header=False)
