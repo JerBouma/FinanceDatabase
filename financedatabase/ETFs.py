251f979ed99114bbc03012a8a448a86159d753dc
jeroen <jer.bouma@gmail.com>
2023-03-08 13:17:46 +0100

    Small issue

--- financedatabase/ETFs.py
+++ financedatabase/ETFs.py
@@ -20,7 +20,7 @@ class ETFs(FinanceDatabase):
     ability to select specific ETFs based on the category and/or family.
     """
 
-    FILE_NAME = "etfs.bzb2"
+    FILE_NAME = "etfs.bz2"
 
     def select(
         self,
